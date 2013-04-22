f1c5004d6ce97368b601bc475f62dd68cee154f9
Connie Yuan <conyuan@ubuntu.(none)>
2013-04-21 19:43:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expresso2::Application.config.secret_token = 'be6d67b50235b2e79fa5ce3def888f7b351eec3386b655c6c31eb99b3afbde788c03f96599e7e38ae77d8877d8279ffa3d1a6b0a88ac2290fcdfbdb7b8f34354'
